1c5ca1bb93fd6f4b05060a5c33a7eec59f256043
kacper pirog <kacper.pirog@gmail.com>
2020-11-23 18:32:56 +0100

    refactor imports

--- src/js/components/DatePicker.js
+++ src/js/components/DatePicker.js
@@ -1,6 +1,6 @@
 /* global flatpickr */ 
 
-import BaseWidget from './components/BaseWidget.js';
+import BaseWidget from './BaseWidget.js';
 import utils from '../utils.js';
 import {select, settings} from '../settings.js';
 
